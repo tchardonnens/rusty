dfdca6dcbf2a68c764e1459480e608c557bffb6e
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-08-04 16:57:58 +0200

    (api): stream little videos

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::path::PathBuf;
-
+use actix_files::NamedFile;
+use actix_web::{web, App, Error, HttpServer};
 use clap::{arg, Command};
+use std::path::PathBuf;
 
 fn cli() -> Command {
     Command::new("rusty")
@@ -14,7 +15,13 @@ fn cli() -> Command {
         .subcommand(Command::new("stop").about("Stop the server"))
 }
 
-fn main() {
+async fn index(path: String) -> Result<NamedFile, Error> {
+    let path = PathBuf::from(path);
+    Ok(NamedFile::open(path)?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     let matches = cli().get_matches();
     match matches.subcommand() {
         Some(("start", sub_matches)) => {
@@ -24,6 +31,14 @@ fn main() {
             if let Some(s) = path.to_str() {
                 let s = s.to_string();
                 println!("Streaming {}", s);
+                let path_string = s.clone();
+                HttpServer::new(move || {
+                    let path_clone = path_string.clone();
+                    App::new().route("/", web::get().to(move || index(path_clone.clone())))
+                })
+                .bind("127.0.0.1:8080")?
+                .run()
+                .await?;
             } else {
                 println!("Path is not valid UTF-8");
             }
@@ -33,4 +48,6 @@ fn main() {
         }
         _ => unreachable!(),
     }
+
+    Ok(())
 }
